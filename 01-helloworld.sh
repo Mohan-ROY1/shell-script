11233f013dcd1670e3c74e218addeb706cdc4476
mohan <gulivindalamohan2786@gmail.com>
2023-08-27 16:02:53 +0530

    This is my first program

---
+++ 01-helloworld.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Hello world, I am learning DEVOPS with AWS."
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1 : Hii $PERSON2 good morning"
+echo "$PERSON2 : Good Morning $PERSON"
+echo "$PERSON1 : How are you $PERSON2"
+echo "$PERSON2 : I am good,What about you $PERSON1"
