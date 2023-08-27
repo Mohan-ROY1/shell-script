107658fa781e8b40d7712a8f843b5002f99ee1c6
mohan <gulivindalamohan2786@gmail.com>
2023-08-27 21:18:50 +0530

    variable

--- 12-functions.sh
+++ 12-functions.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
-#This is examle for functions
-DATE=$(date+%F)
-SCRIPT_NAME=$0
-LOG_FILE = /tmp/$SCRIPT_NAME-$DATE.LOG
 
+# our program goal is to install mysql
+
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+# this function should validate the previous command and inform user it is success or failure
 VALIDATE(){
-    if [$1 -ne 0]
+    #$1 --> it will receive the argument1
+    if [ $1 -ne 0 ]
     then
-        echo "$2 of MYSQL is error"
+        echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
-        echo "$2 of MYSQL is success"
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
 
-USER_ID=$(id -u)
-if [ $USER_ID -ne 0 ]
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
 then
-    echo "ERROR :: Please rin this script with root access"
+    echo "ERROR:: Please run this script with root access"
     exit 1
+# else
+#     echo "INFO:: You are root user"
 fi
-yum install mysql -y &>>$LOG_FILE
-VALIDATE $? "mysql installing"
-yum install posddtfix -y &>>$LOG_FILE
-VALIDATE $? "postfix installing"
+
+# it is our responsibility again to check installation is success or not
+yum install mysql -y &>>$LOGFILE
+
+VALIDATE $? "Installing MySQL"
+
+yum install posddtfix -y &>>$LOGFILE
+
+VALIDATE $? "Installing postfix"
