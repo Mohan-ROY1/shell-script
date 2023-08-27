495af0e0da9ac205c273ead3ff8a85bff519d30a
mohan <gulivindalamohan2786@gmail.com>
2023-08-27 18:32:29 +0530

    variables

---
+++ 11-postfix.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Our scriptin is install POSTFIX for GMAIL
+USER_ID=$(id -u)
+if [$USER_ID -ne 0]
+then
+    echo "ERROR :: Please run this script with root access"
+    exit 1
+fi
+yum install postfix -y
+if [$? -ne 0]
+then
+    echo "Installation of postfix is error"
+    exit 1
+else
+    echo "postfix installation is success"
+fi
