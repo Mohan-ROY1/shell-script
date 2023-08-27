09656080eece34098b86b89988b6d4c33a96d6e5
mohan <gulivindalamohan2786@gmail.com>
2023-08-27 21:16:39 +0530

    variable

--- 12-functions.sh
+++ 12-functions.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 #This is examle for functions
-DATE=$(date + %F)
+DATE=$(date+%F)
 SCRIPT_NAME=$0
-LOG_FILE = /tmp/$SCRIPT_NAME - $DATE.LOG
-USER_ID=$(id -u)
+LOG_FILE = /tmp/$SCRIPT_NAME-$DATE.LOG
 
 VALIDATE(){
-    if [$? -ne 0]
+    if [$1 -ne 0]
     then
-        echo "Installation of MYSQL is error"
+        echo "$2 of MYSQL is error"
         exit 1
     else
-        echo "Installation of MYSQL is success"
+        echo "$2 of MYSQL is success"
 }
-if [$? -ne 0]
+
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]
 then
     echo "ERROR :: Please rin this script with root access"
     exit 1
 fi
 yum install mysql -y &>>$LOG_FILE
 VALIDATE $? "mysql installing"
-yum install postfix -y &>>$LOG_FILE
+yum install posddtfix -y &>>$LOG_FILE
 VALIDATE $? "postfix installing"
