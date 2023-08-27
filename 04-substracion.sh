4cb1bdd58fb9b71027504d7b73515eb5ad875dcf
mohan <gulivindalamohan2786@gmail.com>
2023-08-27 18:13:08 +0530

    variables

---
+++ 04-substracion.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# this is scripting for Substraction
+NUMBER1=$1
+NUMBER2=$2
+SUB=$(NUMBER1-NUMBER2)
+echo "Substraction of two numbers is : $SUB"
---
+++ 05-multipli.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# this is scripting for multi
+NUMBER1=$1
+NUMBER2=$2
+MULTI=$(NUMBER1+NUMBER2)
+echo "Addition of two numbers is : $MULTI"
