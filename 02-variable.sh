bb62b26b5f092ce94e6754a56e30314cfbed8f61
mohan <gulivindalamohan2786@gmail.com>
2023-08-27 17:44:42 +0530

    variables

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # shell script run the command inside the $() and stores the output into variable called data.
 DATE=$(date)
-echo "Hello, this script is execute at time stamp : $date"
+echo "Hello, this script is execute at time stamp : $DATE"
