33574a86e7a216762fe63a486ae7bce6bc1bc976
mohan <gulivindalamohan2786@gmail.com>
2023-08-31 10:51:56 +0530

    shell script

--- 15-loos2.sh
+++ 15-loos2.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
-# this scripting for installation purpose
 
 DATE=$(date +%F)
-echo "$DATE"
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "Installing $2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "Installing $2 ... $G SUCCESS $N"
+    fi
+}
+for i in $@
+do
+    yum list installed $i &>>$LOGFILE
+    if [ $? -ne 0 ]
+    then
+        echo "$i is not installed, let's install it"
+        yum install $i -y &>>$LOGFILE
+        VALIDATE $? "$i"
+    else
+        echo -e "$Y $i is already installed $N"
+    fi
+done
